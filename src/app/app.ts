5c4c6c641da904cca6fcdb75e739ec2be392cd7b
Khatiwada <saugat.khatiwada@sogeti.com>
2026-08-28 10:36:43 +0200

    added multiple components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { About } from './about/about';
+import { Home } from './home/home';
+import { Contact } from './contact/contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Home, About, Contact],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
