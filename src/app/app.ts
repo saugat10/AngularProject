15ccc21f3bb382f9ab59dd991373f1ff0e23224e
Khatiwada <saugat.khatiwada@sogeti.com>
2026-08-28 10:14:48 +0200

    added existing files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('my-first-app');
+  protected readonly title = signal('Saugat Angular');
 }
